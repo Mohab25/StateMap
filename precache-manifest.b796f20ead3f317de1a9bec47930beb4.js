40aec14edec4e214d380dc6fba7d5f13e7942a7a
mohab <homab3@gmail.com>
2020-07-02 16:37:28 +0200

    Updates

---
+++ precache-manifest.b796f20ead3f317de1a9bec47930beb4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80c54576cdb54278f32d858d300d5876",
+    "url": "/StateMap/index.html"
+  },
+  {
+    "revision": "ad15c92ea19adf478a97",
+    "url": "/StateMap/static/css/2.7f75bbe2.chunk.css"
+  },
+  {
+    "revision": "c5337d3d88a89cc9dceb",
+    "url": "/StateMap/static/css/main.d28b2546.chunk.css"
+  },
+  {
+    "revision": "ad15c92ea19adf478a97",
+    "url": "/StateMap/static/js/2.1f19ab0c.chunk.js"
+  },
+  {
+    "revision": "ac5d9a2ccd3ba848587d2a460762f2f0",
+    "url": "/StateMap/static/js/2.1f19ab0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5337d3d88a89cc9dceb",
+    "url": "/StateMap/static/js/main.523666f7.chunk.js"
+  },
+  {
+    "revision": "3a12e927428e3c2ad2ff",
+    "url": "/StateMap/static/js/runtime-main.7c9d901d.js"
+  },
+  {
+    "revision": "baa0fbd305d69958efc831e23016a3a7",
+    "url": "/StateMap/static/media/Dark.baa0fbd3.jpg"
+  },
+  {
+    "revision": "11e2b4786004902fc5a19828fd1ff316",
+    "url": "/StateMap/static/media/Light.11e2b478.jpg"
+  },
+  {
+    "revision": "beed083d928e64a5b57e807e296369cf",
+    "url": "/StateMap/static/media/Topo.beed083d.jpg"
+  }
+]);
