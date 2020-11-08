e1a7aad43d453b8aa692d3af3b16aeb8bd32b990
mohab <homab3@gmail.com>
2020-11-08 23:06:44 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/StateMap/precache-manifest.8272bdc47a25bd7a8931dba548bc9564.js"
+  "/StateMap/precache-manifest.56127ad717e5848fd703b74819ba02cf.js"
 );
 
 self.addEventListener('message', (event) => {
